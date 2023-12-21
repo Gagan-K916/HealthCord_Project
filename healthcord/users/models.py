c41aa2c8138a72a65bd70dede77be22bd34a15d8
Gagan Deep <gagan.kulal916@gmail.com>
2023-12-21 16:31:32 +0530

    Second Commit

--- healthcord/users/models.py
+++ healthcord/users/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+# creating the patient database
+class Patient(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    gender = models.CharField(max_length=10)
+
+    def __str__(self):
+        return self.user.username
+
--- healthcord/users/urls.py
+++ healthcord/users/urls.py
@@ -2,5 +2,6 @@ from django.urls import path, include
 from . import views
 
 urlpatterns = [
-    path('', views.home, name = 'home-page')
+    path('', views.home, name = 'home-page'),
+    path('register/', views.register , name = 'register-page')
 ]
